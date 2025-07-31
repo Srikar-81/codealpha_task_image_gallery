161ab891da1367a2ae503347fce7491d09b0e2ce
Srikar_chittipanthula <chsrikar436@gmail.com>
2025-07-31 16:56:06 +0530

    script.js

--- codealpha_imagegallery/script.js
+++ codealpha_imagegallery/script.js
@@ -2,13 +2,13 @@ function fun1(){
     document.getElementById("photo").src="./photos/1.jpg"
 }
 function fun2(){
-    document.getElementById("photo").src="./photos/2.jpg"
+    document.getElementById("photo").src="./photos/2.png"
 }
 function fun3(){
-    document.getElementById("photo").src="./photos/3.jpg"
+    document.getElementById("photo").src="./photos/3.png"
 }
 function fun4(){
-    document.getElementById("photo").src="./photos/4.jpg"
+    document.getElementById("photo").src="./photos/4.png"
 }
 function fun5(){
     document.getElementById("photo").src="./photos/5.jpg"
@@ -17,28 +17,28 @@ function fun6(){
     document.getElementById("photo").src="./photos/6.jpg"
 }
 function fun7(){
-    document.getElementById("photo").src="./photos/7.jpg"
+    document.getElementById("photo").src="./photos/7.png"
 }function fun8(){
-    document.getElementById("photo").src="./photos/8.jpg"
+    document.getElementById("photo").src="./photos/8.png"
 }
 function fun9(){
     document.getElementById("photo").src="./photos/9.jpg"
 }
 function fun10(){
-    document.getElementById("photo").src="./photos/10.jpg"
+    document.getElementById("photo").src="./photos/10.png"
 }
 function fun11(){
     document.getElementById("photo").src="./photos/11.jpg"
 }
 function fun12(){
-    document.getElementById("photo").src="./photos/12.jpg"
+    document.getElementById("photo").src="./photos/12.png"
 }
 function fun13(){
     document.getElementById("photo").src="./photos/13.jpg"
 }
 function fun14(){
-    document.getElementById("photo").src="./photos/14.jpg"
+    document.getElementById("photo").src="./photos/14.png"
 }
 function fun15(){
-    document.getElementById("photo").src="./photos/15.jpg"
+    document.getElementById("photo").src="./photos/15.png"
 }
